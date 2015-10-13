af2c4ff1d610f5eaa4ea1bd8e79532c0962bbe46
Diogo Luis <diogo.silva@ebc.com.br>
2015-10-13 13:03:26 -0300

    Implementing shell execution to update project in remote server

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var execSsh = require('ssh-exec');
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	console.log(req.body);
-  res.send("OK!");
+	var db = req.db;
+	db.collection('maps').find({"project_id": req.body.project_id}).toArray(function(e,result){
+		if(e) next(e);
+		console.log(config.user_ssh+"@"+result[0].server);
+		execSsh(result[0].command, config.user_ssh+"@"+result[0].server).pipe(process.stdout);
+		res.send("OK!"); 
+	});
+ 
 });
 module.exports = router;
